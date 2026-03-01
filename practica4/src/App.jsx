86c5d2237d6be645cce247b7dfcf8669d69fe756
arimartzbrito25 <arimartzbrito25@gmail.com>
2026-03-01 16:08:52 -0600

    16. Manda a llamar a la función handleSubmit desde el evento onSubmit del formulario.

---
+++ practica4/src/App.jsx
@@ -0,0 +1,23 @@
+import { BudgetForm } from './components/BudgetForm'
+import { useContext } from "react"
+import { BudgetStateContext } from "./context/BudgetContext"
+
+function App() {
+    const state = useContext(BudgetStateContext)
+    const isValidBudget = state.budget > 0
+  return (
+    <>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+          Planificador de gastos
+        </h1>
+      </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        {isValidBudget? <h1>Detalle de gastos</h1> : <BudgetForm />}
+      </div>
+    </>
+  )
+}
+
+export default App
